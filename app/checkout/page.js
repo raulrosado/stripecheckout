642181bebb1b96af431b38ff87d03df34ec91cd8
Raul Remedios Rosado <62999152+raulrosado@users.noreply.github.com>
2023-12-17 17:36:14 -0600

    git init

---
+++ app/checkout/page.js
@@ -0,0 +1,10 @@
+import Image from 'next/image'
+
+export default function checkout() {
+    return (<>
+            <div>
+                <h1>Checkout</h1>
+            </div>
+        </>    
+    )
+}
