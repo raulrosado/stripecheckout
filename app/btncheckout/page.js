5c2bd677694c0064b3fad0d0b57ea5e68a608982
Raul Remedios Rosado <62999152+raulrosado@users.noreply.github.com>
2024-01-01 15:09:59 -0600

    Add:new checkout

---
+++ app/btncheckout/page.js
@@ -0,0 +1,11 @@
+"use client";
+import Image from 'next/image'
+import React, { useEffect, useState } from "react";
+
+export default function btncheckout() {
+    return (
+        <>
+            <h1>Btn Checkout</h1>
+        </>
+    )
+}
